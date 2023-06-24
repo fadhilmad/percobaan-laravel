fb2fece466ed9943a2e6fe0f7be21939e749d824
fadhilmad <ffaza46@gmail.com>
2023-06-24 16:46:31 +0700

    perubahan text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -287,6 +287,16 @@
                         <h2>Project fitur</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                             labore et dolore magna aliqua.</p>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
+
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua.</p>
                     </div>
 
 
